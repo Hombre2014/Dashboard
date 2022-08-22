7730826180272329fc5c7024b25e39b3dd721ff7
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-08-22 17:53:52 +0200

    Setup folder structure and import data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className='underline text-3xl'>App</h1>
   )
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
+import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
